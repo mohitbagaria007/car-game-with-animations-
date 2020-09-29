a445ba3bba820faca676f59708db1913c594a1a0
mohitbagaria007 <64145048+mohitbagaria007@users.noreply.github.com>
2020-09-29 18:51:53 +0530

    Add files via upload

---
+++ CarRacingStage1.5-master/sketch.js
@@ -0,0 +1,39 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+function preload() { 
+  c1 = loadImage("images/car1.png"); 
+  c2 = loadImage("images/car2.png"); 
+  c3 = loadImage("images/car3.png"); 
+  c4 = loadImage("images/car4.png"); 
+  tra = loadImage("images/track.jpg");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start(); 
+  
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
